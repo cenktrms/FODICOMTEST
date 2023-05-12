3047f8796b373d89741fc1e932ad1329f18312bd
Cenk Töremiş <cenk@interpacs.com>
2023-05-12 11:49:21 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using FellowOakDicom;
+using FellowOakDicom.Imaging;
+using FellowOakDicom.Imaging.Codec;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DicomSetupBuilder()
+              .RegisterServices(s => s.AddFellowOakDicom().AddTranscoderManager<FellowOakDicom.Imaging.NativeCodec.NativeTranscoderManager>())
+              .RegisterServices(s => s.AddFellowOakDicom().AddImageManager<WinFormsImageManager>())
+              .SkipValidation()
+              .Build();
+            DicomImage dicomImage;
+            DicomFile dicomFile = DicomFile.Open(@"DICOM\1.dcm");
+            dicomImage = new DicomImage(dicomFile.Dataset, 1);
+            var image = dicomImage.RenderImage(0);
+            image.AsClonedBitmap().Save(@"DICOM\123.jpeg", ImageFormat.Jpeg);
+        }
+    }
+}
